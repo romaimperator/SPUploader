88bf43ce151babc9023b2139ed5a5f6dd443750f
Dan Fox <romaimperator@gmail.com>
2010-10-22 13:22:02 -0500

    work in progress of making rendering and template merging

--- Page.rb
+++ Page.rb
@@ -1,18 +1,19 @@
 
 
-
 class Page
   NEWLINE_TAG = "<newline>"
   
   # tag syntax: {name /} or {name}value{/name}
   # matches {name /} where $1 is 'name'
-  TAG = /\{([^}\/ ]+)\s*\/\}/  
+  TAG = /\A(\{([^}\/ ]+)\s*\/\})/
   # matches opening tag of {name}value{/name} where $1 is 'name'
-  OPEN_TAG = /\{([^}\/ ]+)\}/
+  OPEN_TAG = /\A(\{([^}\/ ]+)\})/
   # matches closing tag of {name}value{/name} where $1 is 'name'
-  CLOSE_TAG = /\{\/([^}\/ ]+)\}/
+  CLOSE_TAG = /\A(\{\/([^}\/ ]+)\})/
+  # matches all text until a {
+  VALUE_TEXT = /\A(([^{]|\s)+)(\{|\Z)/
   
-  attr_accessor :name, :code, :file, :tags, :values
+  attr_accessor :name, :code, :file, :tags, :values, :html, :merged_values
   
   def initialize(name, filename)
     @name = name
@@ -24,6 +25,12 @@ class Page
       exit 1
     end
     @tags, @values = parse_tags
+    @html = ""
+    @merged_values = {}
+  end
+  
+  def write_html(html)
+    @html = html
   end
   
   # Returns the page code as a string with newlines represented as '<newline>'
@@ -56,19 +63,65 @@ class Page
   #  return @tags.include?(t)
   #end
   
+  # Adds the values found in 'template' to the matching tags in 'values' and
+  #  returns the new values hash
+  # TODO: unfortunately gonna be much more complicated
+  #   -shouldn't be merged values
+  #   -should produce the code stepping through the root value replacing tags
+  #     that have values in 'template'
+  #   -probably use regex to match {tag /} and replace with value
+  #   -should output string of all merges
+  #   -actually a structure of tags made at same time as parsing could assist
+  def merge_template_values(template)
+    @merged_values = @values.merge(template.values)
+  end
+  
+  # Writes values into code returning the string created
+  def merge_values_and_code
+    puts get_tag_regex(values.keys[0])
+  end
+  
+  # Produces regex to match a value opening tag from tag name
+  def get_open_tag_regex(tag)
+    return Regexp.new("\\{#{tag}\\}")
+  end
+  
+  # Produces regex to match a value closing tag from tag name
+  def get_close_tag_regex(tag)
+    return Regexp.new("\\{/#{tag}\\}")
+  end
+  
+  # Produces regex to match a non-value tag from tag name
+  def get_tag_regex(tag)
+    return Regexp.new("\\{#{tag} /\\}")
+  end
+  
+  def get_open_tag(tag)
+    return "{#{tag}}"
+  end
+  def get_close_tag(tag)
+    return "{/#{tag}}"
+  end
+  def get_tag(tag)
+    return "{#{tag} /}"
+  end
+  
   # Proceeds through all of the lines and pulls out the tags that exist and
   #  values accociated (if there are any) and returns the list of tags found and
   #  a hash of {tag => value} for tags that have values
   def parse_tags
     tags = []
     values = {}
-    open_tags = []
+    open_tags = ['root']
     @code.each do |line|
       tags, values, open_tags = parse_line(line, tags, values, open_tags)
     end
+    #puts open_tags.inspect
+    open_tags.slice!(0..-1)
+    #puts open_tags.inspect
     if not open_tags.empty?
       puts "Error: missing the following #{open_tags.size} closing tags"
-      puts open_tags
+      puts open_tags.inspect
       exit 1
     end
     return tags, values
@@ -79,43 +132,68 @@ class Page
   #  returns an array of tags, a hash containing the tags with values, an array
   #  of open tags
   def parse_line(line, tags, values, open_tags)
+    #puts "name:#{@name} line:'#{line}'"
     if line.match(TAG)
-      tags = handle_adding_tag($1, tags)
+      tags = handle_adding_tag($2, tags)
+      if not open_tags.empty?
+        values = handle_add_line_to_value(get_tag($2), open_tags.last, values)
+      end
     elsif line.match(OPEN_TAG)
-      open_tags.push($1)
-      tags = handle_adding_tag($1, tags)
+      puts "pushing:#{$2}"
+      open_tags.push($2)
+      tags = handle_adding_tag($2, tags)
     elsif line.match(CLOSE_TAG)
-      open_tags = handle_close_tag($1, open_tags)
+      values, open_tags = handle_close_tag($2, values, open_tags)
+    elsif line.match(VALUE_TEXT)
+      if not open_tags.empty?
+        values = handle_add_line_to_value($1, open_tags.last, values)
+      end
+    else
+      puts "Error: didn't match '#{line}'"
+      exit 1
     end
     
-    values = handle_add_line_to_values(line, values, open_tags)
-    
-    return tags, values, open_tags
+    whole = $1
+    if line.sub(whole, '') != nil and line.sub(whole, '') != ""
+      return parse_line(line.sub(whole,''), tags, values, open_tags)
+    else    
+      return tags, values, open_tags
+    end
   end
   
+  # Adds the string 'line' to the hash value of 't' in the hash 'values'
+  def handle_add_line_to_value(line, t, values)
+    if values[t] == nil
+      values[t] = line
+    else
+      values[t] += line
+    end
+    return values
+  end
   # Checks if the closing tag is expected and if so pops the tag off the stack
   #  and returns the stack
-  def handle_close_tag(tag, open_tags)
+  def handle_close_tag(tag, values, open_tags)
     if open_tags.last == tag
+      puts "poping:#{tag}"
       open_tags.pop
+      if not open_tags.empty?
+        values = handle_add_line_to_value(get_tag(tag), open_tags.last, values)
+      end
+      puts values.inspect
     else
       puts "Error: closing tag #{open_tags.first} expected but found #{tag}"
       exit 1
     end
-    return open_tags
+    return values, open_tags
   end
   
   # Adds the line to any value that has an open tag except for when the line is
   #  the open tag for the value
   def handle_add_line_to_values(line, values, open_tags)
-    line.match(OPEN_TAG)
+    #line.match(OPEN_TAG)
     open_tags.each do |t|
-      next if t == $1
-      if values[t] == nil
-        values[t] = line
-      else
-        values[t] += line
-      end
+      #next if t == $1
+      values = handle_add_line_to_value(line, t, values)
     end
     return values
   end
--- PageTemplate.rb
+++ PageTemplate.rb
@@ -2,9 +2,25 @@
 require 'Page'
 
 class PageTemplate < Page
+  attr_accessor :parent, :children
   
   def initialize(name, filename)    
     super(name, filename)
+    @parent = nil
+    @children = []
   end
   
+  # Generates the HTML for this template and all sub templates.
+  def render
+    @children.each do |c|
+      next unless c.is_a?(PageTemplate)
+      merge_template_values(c)
+      puts "'#{@merged_values.inspect}'"
+      merge_values_and_code()
+    end
+  end
+  
+  def add_child(template)
+    @children.push(template) unless @children.include?(template)
+  end
 end
